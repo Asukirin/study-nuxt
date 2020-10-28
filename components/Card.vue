7e3d9a1159a2eec8104e21b33093d5b55dfc4471
Asukirin <asukiri.liar@gmail.com>
2020-10-29 00:23:12 +0900

    #2完了
    評価の星の表示切替の実装

--- components/Card.vue
+++ components/Card.vue
@@ -1,23 +1,23 @@
 <template>
-  <article class="w-64 h-64 bg-white rounded m-8 border shadow-md card">
+  <article class="w-64 h-64 bg-white rounded m-8 border shadow-md">
     <header class="h-16 pl-4 pt-3 pb-2">
-      <h3 class="title">{{ title }}</h3>
-      <p class="flex">
+      <h3>{{ title }}</h3>
+      <p class="flex star">
         <span
-          class="star-active value"
+          class="star-active"
           v-for="activeStar in value"
           :key="`activeStar-${activeStar}`"
           >★</span
         >
         <span
-          class="star-inactive value"
+          class="star-inactive"
           v-for="inactiveStar in 5 - value"
           :key="`inactiveStar-${inactiveStar}`"
           >★</span
         >
       </p>
     </header>
-    <p class="overflow-x-auto h-48 px-4 py-2 border-t message">
+    <p class="overflow-x-auto h-48 px-4 py-2 border-t">
       {{ message }}
     </p>
   </article>
@@ -42,12 +42,16 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .star-active {
   color: #ffcc33;
 }
 
 .star-inactive {
   color: #c0c0c0;
+}
+
+.hide-star .star {
+  display: none;
 }
 </style>
---
+++ components/hide_button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    @click="handleClick"
+    class="border border-black rounded-full py-2 px-4"
+  >
+    評価表示切替
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick(e) {
+      this.$emit("click", e);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
