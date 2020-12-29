9e94e3ead4923399cbd40f922bba8880ef0c46e2
Asukirin <asukiri.liar@gmail.com>
2020-12-29 23:05:42 +0900

    Add:表示するレビューの数を選択する機能の追加
    テキストボックス内の数字で制御。
    
    Rename:Card.vueからReviewCard.vueへ変更
    
    HTMLのタグと被ることを避ける為。
    
    Fix:v-bindを省略
    
    短縮記法とそうでないものが混ざっていた為、短縮記法に統一。

---
+++ components/ReviewCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <article class="w-64 h-64 bg-white rounded m-8 border shadow-md">
+    <header class="h-16 pl-4 pt-3 pb-2">
+      <h3>{{ title }}</h3>
+      <p class="flex star">
+        <span
+          class="star-active"
+          v-for="activeStar in value"
+          :key="`activeStar-${activeStar}`"
+          >★</span
+        >
+        <span
+          class="star-inactive"
+          v-for="inactiveStar in 5 - value"
+          :key="`inactiveStar-${inactiveStar}`"
+          >★</span
+        >
+      </p>
+    </header>
+    <p class="overflow-x-auto h-48 px-4 py-2 border-t scroll-none">
+      {{ message }}
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.star-active {
+  color: #ffcc33;
+}
+
+.star-inactive {
+  color: #c0c0c0;
+}
+
+.hide-star .star {
+  display: none;
+}
+
+.scroll-none {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+.scroll-none::-webkit-scrollbar {
+  display: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <hideButton @click="hideStar = !hideStar" />
+    <input type="number" min="1" v-model.number="minViewValue" />件目から
+    <input type="number" min="1" v-model.number="maxViewValue" />件目まで
     <div class="flex flex-wrap">
       <card
         class="card"
-        v-bind:class="{ 'hide-star': hideStar }"
-        v-for="review in reviews"
+        :class="{ 'hide-star': hideStar }"
+        v-for="review in limitCount"
         :title="review.title"
         :message="review.message"
-        :value="review.value"
+        :value="review.rate"
         :key="review.id"
       />
     </div>
@@ -16,101 +18,111 @@
 </template>
 
 <script>
-import Card from "~/components/Card.vue";
+import Card from "~/components/ReviewCard.vue";
 import hideButton from "~/components/HideButton.vue";
 
 export default {
   components: { Card, hideButton },
   data() {
     return {
+      minViewValue: 1,
+      maxViewValue: 10,
       hideStar: false,
       reviews: [
         {
           title: "カードタイトル1",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 3,
+          rate: 3,
           id: 1,
         },
         {
           title: "カードタイトル2",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 1,
+          rate: 1,
           id: 2,
         },
         {
           title: "カードタイトル3",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 4,
+          rate: 4,
           id: 3,
         },
         {
           title: "カードタイトル4",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 2,
+          rate: 2,
           id: 4,
         },
         {
           title: "カードタイトル5",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 5,
+          rate: 5,
           id: 5,
         },
         {
           title: "カードタイトル6",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 4,
+          rate: 4,
           id: 6,
         },
         {
           title: "カードタイトル7",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 2,
+          rate: 2,
           id: 7,
         },
         {
           title: "カードタイトル8",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 1,
+          rate: 1,
           id: 8,
         },
         {
           title: "カードタイトル9",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 3,
+          rate: 3,
           id: 9,
         },
         {
           title: "カードタイトル10",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 3,
+          rate: 3,
           id: 10,
         },
         {
           title: "カードタイトル11",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 1,
+          rate: 1,
           id: 11,
         },
         {
           title: "カードタイトル12",
           message:
             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod assumenda libero amet odit obcaecati sed facilis? Non voluptatem expedita molestiae asperiores repudiandae laudantium blanditiis velit harum explicabo quidem? Et, delectus?",
-          value: 4,
+          rate: 4,
           id: 12,
         },
       ],
     };
+  },
+  computed: {
+    limitCount() {
+      let minNum = this.minViewValue;
+      let maxNum = this.maxViewValue;
+
+      return this.reviews.slice(minNum - 1, maxNum);
+    },
   },
 };
 </script>
